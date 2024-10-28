4f8e8ab8c63dbcc122fae9ab753f599e6c01f42f
Sormailgu <sormailgu@gmail.com>
2024-10-28 09:27:23 +0800

    Update appsetting and programs

--- Program.cs
+++ Program.cs
@@ -21,10 +21,14 @@ builder.Services.AddSwaggerGen();
 // Register Kiota handlers to the dependency injection container
 builder.Services.AddKiotaHandlers();
 
+// Read configuration values
+var financialServiceBaseUrl = builder.Configuration["ApiSettings:FinancialServiceBaseUrl"];
+var configureServiceBaseUrl = builder.Configuration["ApiSettings:ConfigureServiceBaseUrl"];
+
 // Register the factory for the FinancialService client
 builder.Services.AddHttpClient<FinancialServiceClientFactory>((sp, client) => {
     // Set the base address for the FinancialService API
-    client.BaseAddress = new Uri("http://localhost:5180/api");
+    client.BaseAddress = new Uri(financialServiceBaseUrl);
 }).AttachKiotaHandlers(); // Attach the Kiota handlers to the HTTP client
 
 // Register the FinancialService client
@@ -33,24 +37,32 @@ builder.Services.AddTransient(sp => sp.GetRequiredService<FinancialServiceClient
 // Register the factory for the ConfigureService client
 builder.Services.AddHttpClient<ConfigureServiceClientFactory>((sp, client) => {
     // Set the base address for the ConfigureService API
-    client.BaseAddress = new Uri("http://localhost:5102");
+    client.BaseAddress = new Uri(configureServiceBaseUrl);
 }).AttachKiotaHandlers(); // Attach the Kiota handlers to the HTTP client
 
 // Register the ConfigureService client
 builder.Services.AddTransient(sp => sp.GetRequiredService<ConfigureServiceClientFactory>().GetClient());
-
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
